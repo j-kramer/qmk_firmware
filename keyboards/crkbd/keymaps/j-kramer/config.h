10ad05b184374238b6c2bdfb214fa3c9b761e47c
Jan Kramer <6755350+j-kramer@users.noreply.github.com>
2021-10-23 20:20:41 +0200

    fixed build of crkbd and lets split

--- keyboards/crkbd/keymaps/j-kramer/config.h
+++ keyboards/crkbd/keymaps/j-kramer/config.h
@@ -20,16 +20,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* Use I2C or Serial, not both */
-
-// #define USE_SERIAL
-#define USE_I2C
+//#define USE_MATRIX_I2C
 
 /* Select hand configuration */
 
 // #define MASTER_LEFT
-// #define _MASTER_RIGHT
+// #define MASTER_RIGHT
 #define EE_HANDS
 
-/* Keymap specific options */
-#define ONESHOT_TAP_TOGGLE 3
+#define USE_SERIAL_PD2
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 100
+
+#ifdef RGBLIGHT_ENABLE
+    #undef RGBLED_NUM
+    #define RGBLIGHT_ANIMATIONS
+    #define RGBLED_NUM 27
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
--- keyboards/lets_split/keymaps/j-kramer/keymap.c
+++ keyboards/lets_split/keymaps/j-kramer/keymap.c
@@ -1,8 +1,9 @@
 #include QMK_KEYBOARD_H
-#include "action_layer.h"
 
-extern keymap_config_t keymap_config;
-
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
 #define _QWERTY 0
 #define _COLEMAK 1
 #define _DVORAK 2
@@ -33,7 +34,6 @@ enum custom_keycodes {
 #define OS_LGUI OSM(MOD_LGUI)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
 /* Qwerty
  * ,-----------------------------------------..-----------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  ||   Y  |   U  |   I  |   O  |   P  | Bksp |
